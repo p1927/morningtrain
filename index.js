60394d75a3f14edaa90984d6f77a6c0a7d61813e
Pratyush <99pratyush@gmail.com>
2018-05-10 21:46:41 +0530

    changes in index

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
+//THis file uses CSV parser for parsing.
+
+//including CSV_Parser module.
 const csv = require('./CSV_Parser/csv_parser');
+
 const path = require('path');
-const filePath = path.join(__dirname, 'CSV_Parser/demo.csv');
+//Adding 500000 Sales Records.csv to path
+const filePath = path.join(__dirname, '500000 Sales Records.csv');
+
 let Parsed = csv.CSV_parser(filePath);
 Parsed.then(function(result) { //process csv contents here
 	console.log(result);
