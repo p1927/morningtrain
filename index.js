62c168ab2a315259c5fb6508c7c04c14fc79d399
Pratyush <99pratyush@gmail.com>
2018-05-10 21:42:07 +0530

    path changes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const csv = require('./CSV_Parser/csv_parser');
 const path = require('path');
-const filePath = path.join(__dirname, 'demo.csv');
+const filePath = path.join(__dirname, 'CSV_Parser/demo.csv');
 let Parsed = csv.CSV_parser(filePath);
 Parsed.then(function(result) { //process csv contents here
 	console.log(result);
